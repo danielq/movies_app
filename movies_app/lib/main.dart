e6444bf6d67be31f759a0f8c0827fc3cb8395dab
daniel <djuanrique@gmail.com>
2025-03-08 11:04:57 -0500

    third_commit

--- movies_app/lib/main.dart
+++ movies_app/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:movies_app/screens/details_screen.dart';
-import 'package:movies_app/screens/home_screen.dart';
+import 'package:movies_app/screens/screens.dart';
 
 void main() => runApp(const MyApp());
 
@@ -10,10 +9,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Peliculas',
-      initialRoute: 'home',
-      routes: {'home': (_) => HomeScreen(), 'details': (_) => DetailsScreen()},
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'Peliculas',
+        initialRoute: 'home',
+        routes: {
+          'home': (_) => HomeScreen(),
+          'details': (_) => DetailsScreen()
+        },
+        // tema principal de la app
+        theme: ThemeData.light()
+            .copyWith(appBarTheme: AppBarTheme(color: Colors.lightBlueAccent)));
   }
 }
