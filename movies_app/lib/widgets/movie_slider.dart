5b682e42c988fb5e1b99ba36f87c2f7ee3a5d90e
daniel <djuanrique@gmail.com>
2025-03-15 11:05:48 -0500

    cmiiit

--- movies_app/lib/widgets/movie_slider.dart
+++ movies_app/lib/widgets/movie_slider.dart
@@ -6,7 +6,6 @@ class MovieSlider extends StatelessWidget {
     return Container(
       width: double.infinity,
       height: 250,
-      color: Colors.red,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
@@ -17,18 +16,48 @@ class MovieSlider extends StatelessWidget {
               style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
           ),
+          SizedBox(height: 5),
           Expanded(
               child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
                   itemCount: 20,
-                  itemBuilder: (_, int index) {
-                    return Container(
-                      width: 130,
-                      height: 190,
-                      color: Colors.green,
-                      margin:
-                          EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-                    );
-                  }))
+                  itemBuilder: (_, int index) => _MoviePoster()))
+        ],
+      ),
+    );
+  }
+}
+
+class _MoviePoster extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 130,
+      height: 190,
+      margin: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+      child: Column(
+        children: [
+          GestureDetector(
+            onTap: () => Navigator.pushNamed(context, 'details',
+                arguments: 'movie-instance'),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: FadeInImage(
+                placeholder: AssetImage('assets/no-image.jpg'),
+                image: NetworkImage('https://placehold.jp/300x400.png'),
+                width: 130,
+                height: 150,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          SizedBox(height: 5),
+          Text(
+            'Star Wars: El retorno de la nueva orden Jedi',
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
+            textAlign: TextAlign.center,
+          )
         ],
       ),
     );
