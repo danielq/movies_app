7b1b8d7bbafc298a3bbef8fcc608edcc55ead1eb
daniel <djuanrique@gmail.com>
2025-03-15 11:22:58 -0500

    commit

--- movies_app/lib/screens/details_screen.dart
+++ movies_app/lib/screens/details_screen.dart
@@ -18,6 +18,21 @@ class DetailsScreen extends StatelessWidget {
 class _CustomAppBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SliverAppBar(
+      backgroundColor: Colors.blue,
+      expandedHeight: 200,
+      floating: false,
+      pinned: true,
+      flexibleSpace: FlexibleSpaceBar(
+        centerTitle: true,
+        titlePadding: EdgeInsets.all(0),
+        title: Container(
+          width: double.infinity,
+          alignment: Alignment.bottomCenter,
+          color: Colors.black12,
+          child: Text('Movie Title'),
+        ),
+      ),
+    );
   }
 }
