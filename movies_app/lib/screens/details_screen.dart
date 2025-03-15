e710ff298eb626c05bd59ef70173b8d3da65d2a0
daniel <djuanrique@gmail.com>
2025-03-15 11:45:25 -0500

    committtt

--- movies_app/lib/screens/details_screen.dart
+++ movies_app/lib/screens/details_screen.dart
@@ -10,7 +10,10 @@ class DetailsScreen extends StatelessWidget {
         ModalRoute.of(context)?.settings.arguments.toString() ?? 'no-movie';
     return Scaffold(
         body: CustomScrollView(
-      slivers: [_CustomAppBar()],
+      slivers: [
+        _CustomAppBar(),
+        SliverList(delegate: SliverChildListDelegate([_PosterAndTitle()]))
+      ],
     ));
   }
 }
@@ -19,7 +22,7 @@ class _CustomAppBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SliverAppBar(
-      backgroundColor: Colors.blue,
+      backgroundColor: Colors.lightBlue,
       expandedHeight: 200,
       floating: false,
       pinned: true,
@@ -32,6 +35,33 @@ class _CustomAppBar extends StatelessWidget {
           color: Colors.black12,
           child: Text('Movie Title'),
         ),
+        background: FadeInImage(
+          placeholder: AssetImage('assets/loading.gif'),
+          image: NetworkImage('https://placehold.jp/300x400.png'),
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+}
+
+class _PosterAndTitle extends StatelessWidget {
+  Widget build(BuildContext context) {
+    final TextTheme textTheme = Theme.of(context).textTheme;
+    return Container(
+      margin: EdgeInsets.only(top: 20),
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: FadeInImage(
+              placeholder: AssetImage('assets/no-image.jpg'),
+              image: NetworkImage('https://placehold.jp/300x400.png'),
+              height: 150,
+            ),
+          )
+        ],
       ),
     );
   }
