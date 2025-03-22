9bbe82161f493de739b84650b9d6f4ef7423ad0c
daniel <djuanrique@gmail.com>
2025-03-22 10:35:53 -0500

    push

--- movies_app/lib/widgets/casting_cards.dart
+++ movies_app/lib/widgets/casting_cards.dart
@@ -8,7 +8,32 @@ class CastingCards extends StatelessWidget {
     return Container(
       width: double.infinity,
       height: 180,
-      color: Colors.red,
+      child: ListView.builder(
+        itemCount: 10,
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (_, int index) => _CastCard(),
+      ),
+    );
+  }
+}
+
+class _CastCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 10),
+      width: 110,
+      height: 100,
+      child: Column(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: FadeInImage(
+                placeholder: AssetImage('assets/no-image.jpg'),
+                image: NetworkImage('https://placehold.jp/200x300.png')),
+          )
+        ],
+      ),
     );
   }
 }
