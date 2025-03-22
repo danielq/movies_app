5b6316a4613cea410830f95a70c9835b8b6ff9db
daniel <djuanrique@gmail.com>
2025-03-22 10:51:11 -0500

    otro jodon mas

---
+++ movies_app/lib/providers/movies_provider.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class MoviesProvider extends ChangeNotifier {
+  MoviesProvider() {
+    print('Movies Provider Inicializado');
+    this.getOnDisplayMovies();
+  }
+  getOnDisplayMovies() async {
+    print('getOnDisplayMovies');
+  }
+}
